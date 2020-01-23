00e5b0fae07855bc65a98e6b122f990e4b5c45c4
Chris Wade <swade781977@yahoo.com>
2020-01-23 03:02:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 def assign_rooms(attendees)
   room = 1
   room_assignments =[]
-  array.each do |name|
+  attendees.each do |name|
     room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
     room += 1
   end
