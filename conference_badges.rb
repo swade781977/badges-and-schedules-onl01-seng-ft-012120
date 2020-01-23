59fcbad996128cb8f68a74c13d66f329eabd30f8
Chris Wade <swade781977@yahoo.com>
2020-01-23 03:02:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,18 +12,18 @@ def assign_rooms(attendees)
   room = 1
   room_assignments =[]
   array.each do |name|
-    room_assignments << "Hello, #{name}! You'll be assigned to room #{count}!"
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
     room += 1
   end
   return room_assignments
 end
 def printer(attendees)
   batch_badge_creator(attendees)
-  badge_array.each do |badge|
+  badges.each do |badge|
     puts badge
   end
   assign_rooms(attendees)
-  rooms_array.each do |room_assignment|
+  room_assignments.each do |room_assignment|
     puts room_assignment
   end
 end
