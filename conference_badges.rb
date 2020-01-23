f124471227522e3b131968a741a984e5382e4136
Chris Wade <swade781977@yahoo.com>
2020-01-23 02:23:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(array)
+  array.each do |name|
+    badges = []
+    badges << "Hello, my name is #{name}."
+  end
+   return badges
+end
+def assign_rooms(array)
+  count = 1
+  rooms = []
+  array.each do |name|
+    rooms << "Hello, #{name}! \n You'll be assigned to room #{count}"
+    count +=1
+  end
+  return rooms
+end
+    
